767a5c0a52f687d21d167ec25285f1fe2cc8d7d3
Erick Adrian Perez <yo@erickperez.com>
2017-01-11 18:52:04 -0700

    Things I forgot

--- client/components/Game.js
+++ client/components/Game.js
@@ -17,48 +17,54 @@ class Game extends React.Component {
     render() {
         return (
             <div className="row">
-                <div className="col l9">
+                <div className="col l12">
                     <h2>Game Portal</h2>
 
                     <div className="row">
                         <div className="col l4">
                             <div className="card">
+                                <div className="card-image">
+                                    <img src="img/reacteroids.png" />
+                                </div>
+                                <div className="card-content">
+                                    <p>Game based on the 80's classic Asteroids</p>
+                                </div>
+                                <div className="card-action">
+                                    <a href="/asteroids">Reacteroids</a>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="col l4">
+                            <div className="card">
+                                <div className="card-image">
+                                    <img src="img/tetris.png" />
+                                </div>
+                                <div className="card-content">
+                                    <p>Play some Tetris мой друг!</p>
+                                </div>
+                                <div className="card-action">
+                                    <a href="/tetris/">Tetris</a>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="col l4">
+                            <div className="card">
+                                <div className="card-image">
+                                    <img src="img/jeopardy.png" />
+                                </div>
+                                <div className="card-content">
+                                    <p>Taa-daa-daaa-da... You know the song. Here's Jeopardy!</p>
+                                </div>
+                                <div className="card-action">
+                                    <a href="https://ccoenraets.github.io/react-trivia/">Jeopardy</a>
+                                </div>
                             </div>
                         </div>
                     </div>
-                </div>
-                <div className="col l3">
-                    <a href="/asteroids" className="game-nav blue-grey darken-2">Asteroids</a>
-                    <a href="/tetris/" className="game-nav deep-orange darken-4">Tetris</a>
-                    <a href="https://ccoenraets.github.io/react-trivia/" onmouseover={this.loadIframe} className="game-nav yellow darken-3">Jeopardy</a>
-                    <a href="/one" className="game-nav light-blue accent-3">One</a>
-                    <a href="/two" className="game-nav green darken-4">Two</a>
                 </div>
             </div>
         )
     }
 }
-
-// const Game = ({ user }) => (
-//     <div className="row">
-//         <div className="col l9">
-//             <h2>Game Portal</h2>
-//             {/* <iframe src="https://ccoenraets.github.io/react-trivia/" width="100%" height="300px"></iframe> */}
-//         </div>
-//         <div className="col l3">
-//             <a href="/asteroids" className="game-nav blue-grey darken-2">Asteroids</a>
-//             <a href="/tetris/" className="game-nav deep-orange darken-4">Tetris</a>
-//             <a className="game-nav yellow darken-3">Jeopardy</a>
-//             <a href="/one" className="game-nav light-blue accent-3">One</a>
-//             <a href="/two" className="game-nav green darken-4">Two</a>
-//         </div>
-//     </div>
-// )
-
-// const mapStateToProps = (state) => {
-//     return { user: state.user }
-// }
-//
-// export default connect(mapStateToProps)(Game)
 
 export default Game;
